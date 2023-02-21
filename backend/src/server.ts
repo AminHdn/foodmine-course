d0bee6f76415fd844c44fa4d5cd590fe356f8803
AminHdn <haidarian.amin@yahoo.com>
2023-02-21 16:58:24 +0330

    copy from git hub

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,41 +1,31 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import path from 'path';
-
-
-import express from 'express';
-import cors from 'cors';
-
+import express from "express";
+import cors from "cors";
 import foodRouter from './routers/food.router';
 import userRouter from './routers/user.router';
 import orderRouter from './routers/order.router';
-
 import { dbConnect } from './configs/database.config';
 dbConnect();
 
-const app =express();
-
+const app = express();
 app.use(express.json());
-
 app.use(cors({
     credentials:true,
     origin:["http://localhost:4200"]
 }));
-// app.get('/people', function (req, res) {
-//     res.send('hello');
-// })
 
-// app.use(express.static('frontend'));
-
-app.use("/api/foods",foodRouter);
-app.use("/api/users",userRouter);
-app.use("/api/orders",orderRouter);
+app.use("/api/foods", foodRouter);
+app.use("/api/users", userRouter);
+app.use("/api/orders", orderRouter);
 
 app.use(express.static('public'));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname,'public', 'index.html'))
 })
-const port =5000;
-app.listen(port,()=>{
-    console.log("website served on http://localhost:"+port);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log("Website served on http://localhost:" + port);
 })
