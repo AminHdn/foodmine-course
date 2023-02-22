18a65682c2dd18527860575476ffa489483447fd
AminHdn <haidarian.amin@yahoo.com>
2023-02-23 00:22:17 +0330

    dist full

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -23,7 +23,7 @@ app.use("/api/orders", orderRouter);
 
 app.use(express.static('public'));
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'))
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
 const port = process.env.PORT || 5000;
